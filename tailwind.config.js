3121d952d3629480fa5721b9961aa10c051d69d3
Ahmed Abdo <devahmedabdo@gmail.com>
2023-04-04 03:47:48 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#F4A424",
+      },
+    },
+  },
+  plugins: [],
+};
